2bacf35ecf144ece7f95e02ee4909f006923aa0e
Jaime de Greiff <alegreiff@gmail.com>
2024-10-15 17:22:03 -0500

    FIXED PATHS

---
+++ src/electron/main.js
@@ -0,0 +1,7 @@
+import {app, BrowserWindow} from 'electron';
+import path from 'path';
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadFile(path.join(app.getAppPath() , '/dist-astro/index.html'));
+})
